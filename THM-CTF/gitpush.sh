6c11c1f60cfba58e063b17b00a4d4359fcbc270f
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-07-06 02:26:43 +0600

    Added another CTF of Try Hack Me

--- THM-CTF/gitpush.sh
+++ THM-CTF/gitpush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull origin master
 git add .
-git commit -m "Added today's todo list and added some changes"
+git commit -m "Added another CTF of Try Hack Me"
 git push -u origin master
